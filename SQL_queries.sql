20550f380ca1eb98b719679ee2d93544cdf6e08a
Luis Alva <96627296+Luis102487@users.noreply.github.com>
2024-04-25 21:53:48 -0400

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -1,1 +1,13 @@
+-- Remove any row Duplicates
 
+-- First let's check if there is any duplicates in the dataset.  
+SELECT
+  *
+FROM (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY company, industry, total_laid_off, `date` ) AS row_num
+  FROM
+    luisalva.lay_offs.lay_offs ) AS duplicates
+WHERE
+  row_num > 1;
