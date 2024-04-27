1729d634eded40d9eeed72190092c201dadaf403
Luis Alva <96627296+Luis102487@users.noreply.github.com>
2024-04-27 17:36:50 -0400

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -1,6 +1,6 @@
--- Remove any row duplicates
+-- 1. Remove row duplicates
 
--- First let's check if there is any duplicates in the dataset. 
+-- Let's check if there is any duplicates in the dataset. 
 -- To do this want to create a staging table where we can work and clean the data.
 -- We also add a row_num column in this new table to help us identify row duplicates.
 CREATE TABLE
@@ -33,4 +33,65 @@ FROM
   luisalva.lay_offs.layoff_staging
 WHERE
   row_num > 1;
+
+-- 2. Fix Structural errors
+-- Now we will look for any misspellings, incongruent naming conventions, improper capitalization for each data column.
+
+-- It looks like the company column has white spaces at the beginning for some of the names. Let's trim those white space.
+UPDATE
+  luisalva.lay_offs.layoff_staging
+SET
+  company = TRIM(company);
+
+-- If we look at industry columns we will find some null and empty values.
+-- Also you will that Crypto has different variations. 
+SELECT
+  DISTINCT industry
+FROM
+  luisalva.lay_offs.layoff_staging
+ORDER BY
+  industry;
+
+-- Let take a look at null and empty values. There are 4 companies with null or empty industry values.
+SELECT
+  *
+FROM
+  luisalva.lay_offs.layoff_staging
+WHERE
+  industry IS NULL
+  OR industry = 'NULL';
+
+-- Lets run a query to see row with the same company name so we can update it with the correct company name.
+SELECT
+  *
+FROM
+  luisalva.lay_offs.layoff_staging
+WHERE
+company = 'Carvana';  
+
+-- We found other records for Carvana and it looks like it belongs to the travel industry. Lets updated the empty record.
+ UPDATE
+  luisalva.lay_offs.layoff_staging
+SET
+  industry = 'Transportation'
+WHERE
+  company = 'Carvana'; 
+  
+
+
+  
+
+-- We need to standardize the Crypto entry. Let's name all Crypto.
+UPDATE
+  luisalva.lay_offs.layoff_staging
+SET
+  industry = 'Crypto'
+WHERE
+  industry IN ('Crypto Currency', 'CryptoCurrency');
+
+
+
+
+
+
 
