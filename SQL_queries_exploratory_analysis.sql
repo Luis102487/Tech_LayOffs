401e5448be9a27c4817fde6d1600d2a4d7e0e79f
Luis Alva <96627296+Luis102487@users.noreply.github.com>
2024-04-28 16:13:11 -0400

    Update SQL_queries_exploratory_analysis.sql

--- SQL_queries_exploratory_analysis.sql
+++ SQL_queries_exploratory_analysis.sql
@@ -1,3 +1,10 @@
+-- Time period of the dataset
+SELECT
+  MIN(date) AS first_date,
+  MAX(date) AS last_date
+FROM
+  luisalva.lay_offs.lay_offs_staging;
+
 -- Company with biggest lay off
 SELECT
   company,
@@ -9,3 +16,8 @@ GROUP BY
 ORDER BY
   SUM(total_laid_off) DESC
 LIMIT 1;
+
+stage
+Companies that went under.
+How many millions were raised by companies that went under.
+fund_raised_milliosn
